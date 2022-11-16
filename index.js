fbe80ff5e0568ad41a1198fb2a2422a6ff7d8713
mahabubr <mahabub.r753@gmail.com>
2022-11-17 03:38:19 +0600

    JWT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
@@ -9,6 +10,16 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader) {
+        res.status(401).send('unauthorized')
+    }
+
+    const token = authHeader.split(' ')[1]
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vlhy1ml.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -16,8 +27,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const appointmentOptionsCollection = client.db('doctorsPortal').collection('appointmentOptions')
-
         const bookingsCollection = client.db('doctorsPortal').collection('bookings')
+        const usersCollection = client.db('doctorsPortal').collection('users')
 
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date
@@ -36,6 +47,14 @@ async function run() {
             res.send(options)
         })
 
+        app.get('/bookings', verifyJWT, async (req, res) => {
+            const email = req.query.email
+
+            const query = { email: email }
+            const bookings = await bookingsCollection.find(query).toArray()
+            res.send(bookings)
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body
             const query = {
@@ -51,6 +70,25 @@ async function run() {
             const result = await bookingsCollection.insertOne(booking)
             res.send(result)
         })
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: "" })
+        })
+
     }
     catch (e) {
         console.log(e);
